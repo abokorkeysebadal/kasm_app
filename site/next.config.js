64e5849c5676d201c4372470cb156c4203cd654e
abokorkeysebadal <163710548+abokorkeysebadal@users.noreply.github.com>
2024-03-28 09:29:42 +0300

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,12 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'abokorkeysebadal
+/
+kasm_app',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://abokorkeysebadal.registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
